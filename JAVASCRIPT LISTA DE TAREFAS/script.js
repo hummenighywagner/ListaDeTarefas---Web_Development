489b02c2ba07b004adbd4b4861f144a164c90711
Hummenighy Wagner <150536805+hummenighywagner@users.noreply.github.com>
2025-01-17 11:04:08 -0300

    Upload de arquivos
    Fiz o upload dos arquivos da aplicação desenvolvida

---
+++ JAVASCRIPT LISTA DE TAREFAS/script.js
@@ -0,0 +1,25 @@
+function addTask(event) {
+event.preventDefault();
+
+let inputTarefa = document.getElementById("inputTask");
+let valorDoInput = inputTarefa.value;
+
+if(inputTarefa.value === "") {
+    message.textContent="O campo não pode estar vazio!"
+    document.getElementById("message").style.color="yellow";
+}else{
+    document.getElementById("message").textContent="Tarefa adicionada com sucesso!"
+    document.getElementById("message").style.color="white";
+    document.getElementById("message").style.fontSize="20px"
+let listaDeTarefas = document.getElementById("tasksList");
+let novaTarefa = document.createElement("li");
+
+novaTarefa.textContent = valorDoInput;
+
+listaDeTarefas.appendChild(novaTarefa)
+
+inputTarefa.value = ""
+
+
+    }
+}
